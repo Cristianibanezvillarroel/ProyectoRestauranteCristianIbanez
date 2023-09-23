09b8e5fe0a9d84e7c453e5acef26af3a90871e1d
unknown <cristian.ibanez@facturaenlinea.cl>
2023-09-23 13:09:39 -0300

    algo

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { BrowserRouter, HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import '/src/components/Estilos.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <HashRouter>
+  <BrowserRouter basename='/ProyectoRestauranteCristianIbanez'>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </HashRouter>
+  </BrowserRouter>
 )
