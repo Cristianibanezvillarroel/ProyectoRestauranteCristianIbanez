69858df9b9fba2d705a182d32361eb974e6375a0
unknown <cristian.ibanez@facturaenlinea.cl>
2023-09-25 17:12:13 -0300

    agregacion de alert en envio de reserva

--- src/routes/Reservas.jsx
+++ src/routes/Reservas.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
-import Button from 'react-bootstrap/Button';
+import {Container, Button , Alert} from 'react-bootstrap';
 
 export const Reservas = () => {
   const allMonthValues = [
@@ -23,6 +23,8 @@ export const Reservas = () => {
 
   const [calendarText, setCalendarText] = useState(`No ha seleccionado ningun dia`);
 
+  const [show, setShow] = useState(false);
+
   const handleDateChange = (value) => {
     setSelectedDate(value);
     setCalendarText(`El dia seleccionado es ${value.toDateString()}`);
@@ -37,6 +39,21 @@ export const Reservas = () => {
     const monthValue = allMonthValues[value.getMonth()];
     setCalendarText(`${monthValue} Mes seleccionado`);
   };
+
+  if (show) {
+    return (
+      <div className="App">
+        <Container className='p-4'>
+          <Alert variant="success" onClose={() => setShow(false)} dismissible >
+            <Alert.Heading>Su reserva ha sido enviada con éxito</Alert.Heading>
+            <p>
+              Cierre esta ventana para volver al menu</p>
+          </Alert>
+        </Container>
+      </div>
+    );
+  }
+
   return (
     <div className="calendar-css">
       <h2 className="calander-details">{calendarText}</h2>
@@ -47,10 +64,10 @@ export const Reservas = () => {
         value={selectedDate}
       />
       <div className="d-grid gap-2">
-      <Button variant="primary" size="lg">
-        {selectedDate? 'Enviar Reserva': 'Debe seleccionar un dia para enviar Reserva'}
-      </Button>
-    </div>
+        <Button onClick={() => setShow(true)} variant="primary" size="lg">
+          {selectedDate ? 'Enviar Reserva' : 'Debe seleccionar un dia para enviar Reserva'}
+        </Button>
+      </div>
     </div>
   )
 }
